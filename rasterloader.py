d4b78bbe5e1e4440b93ae14c9956e15ed4206d84
marsniv <limartinshane@gmail.com>
2025-01-29 14:03:37 -0500

    Update rasterloader.py
    some changes to make it run faster by discarding sessions with insufficient neuron counts before eventizing

--- rasterloader.py
+++ rasterloader.py
@@ -8,7 +8,7 @@ rootdir = '/Users/mli853/Documents/NWB/steinmetz2019/allData'
 #select where you want your numpy files stored
 outputdir = '/Users/mli853/Documents/NWB/steinmetz2019/testsave'
 #select brain area 
-brainarea = "SSp"
+brainarea = "BLA"
 #select bin size (seconds)
 binsize = 0.01
 #only include session with over 100 neurons
@@ -21,16 +21,22 @@ for path, subdirs, files in os.walk(rootdir):
     for file in files:
         #check all of the brainlocation files to see if requested brain area is there
         if "channels.brainLocation.tsv" in file:
+            #turn tsv file into pandas
             location = pd.read_csv(str(os.path.join(path, file)), sep='\t') 
+            #select all brain areas in the session
             unique_values = location['allen_ontology'].unique()
-            count = (location['allen_ontology'] == str(brainarea)).sum()
+            #if requested brain area is among the recorded locations 
             if brainarea in unique_values:
-                #if requested brain area is in that list, then select indices of those neurons
+                #see how many neurons from that brain area where recorded 
                 area = location['allen_ontology'].to_numpy()
-                #tosave = np.array(location.index[location.allen_ontology == brainarea])
-                areas.append(area)
-                saves.append(str(os.path.join(path)))
-                sessions.append(str(os.path.basename(path)))
+                brainareaindices= np.where(area == str(brainarea))
+                channel = np.load(str(str(os.path.join(path)))+"/clusters.peakChannel.npy")
+                clusters = np.where(channel==brainareaindices[0])
+                #if greater than or equal to 128, save those sessions
+                if len(clusters[0]) >= 128:
+                    areas.append(area)
+                    saves.append(str(os.path.join(path)))
+                    sessions.append(str(os.path.basename(path)))
 print("relevant sessions identified")
 
 #go through sessions with the good brain area and save necessary stuff for making np arrays
